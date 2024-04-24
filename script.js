0c0c4ad02a295a926f887eeb9eea1afe27947fdf
Lav <killerboylav4904@gmail.com>
2024-04-24 12:14:16 +0530

    Human Choice function added

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log("Hello");
+
+// let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+// console.log(userChoice);
+
+function getHumanChoice() {
+    let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+    return userChoice;
+}
+
+getHumanChoice();
