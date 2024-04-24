43fb1bad405eac8becc57c952cca44796dac4502
Lav <killerboylav4904@gmail.com>
2024-04-24 12:27:29 +0530

    Computer choice function added

--- script.js
+++ script.js
@@ -5,7 +5,18 @@
 
 function getHumanChoice() {
     let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+    // console.log(userChoice);
     return userChoice;
+    
 }
 
 getHumanChoice();
+
+
+
+let choices = ["rock", "paper", "scissors"]; /*Since I have a bit knowledge about arrays, I'm using it for my convenience */
+function getComputerChoice() {
+    return(choices[Math.floor(Math.random() * 3)]);
+}
+
+getComputerChoice();
