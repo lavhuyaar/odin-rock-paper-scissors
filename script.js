c076dc825c62eeb62a4b5415ad41e7791252f8e9
Lav <killerboylav4904@gmail.com>
2024-04-24 22:43:40 +0530

    Formatting code and making it clean

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
-// console.log("Hello");
-
-// let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
-// console.log(userChoice);
-
-function getHumanChoice() {
-    let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
-    // console.log(userChoice.toLowerCase());
-    let user = userChoice.toLowerCase();
-    // console.log(`User's input - ${user}`);
-    return(user);
-    
-}
-
-// getHumanChoice();
-
-
-
-let choices = ["rock", "paper", "scissors"]; /*Since I have a bit knowledge about arrays, I'm using it for my convenience */
-function getComputerChoice() {
-    let random = choices[Math.floor(Math.random() * 3)];
-    // console.log(`Computer's input - ${random}`);
-    return(random);
-}
-
-// getComputerChoice();
-
-
 
 let humanScore = 0;
 let computerScore = 0; 
 
 
-// let humanSelection = getHumanChoice();           /*Commented it at last coz this was stopping me from getting new values for the playRound function */
-// let computerSelection = getComputerChoice();
+function getHumanChoice() {
+    let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+    let user = userChoice.toLowerCase();
+    return(user);
+    
+}
+
+
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let random = choices[Math.floor(Math.random() * 3)];
+    return(random);
+}
 
 
 function playRound(humanChoice, computerChoice) {
@@ -45,19 +28,6 @@ function playRound(humanChoice, computerChoice) {
         console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
         
 
-    // }else if (humanChoice === "scissors" && computerChoice === "paper") {      /*This looks so messy lmao, would love to know how others did this part */
-    //     console.log("You win! Scissors beat Paper");
-    //     humanScore++;
-    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
-
-
-    // }else if (humanChoice === "paper" && computerChoice === "rock") {
-    //     console.log("You win! Paper beats Rock");
-    //     humanScore++;
-    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
-
-
-    /*I remembered that || can be used for "or" so tried doing this and it makes the code less lengthy. Not deleting the older codes as I would like to remember what code I wrote before xD */
 
     }else if (
             humanChoice === "scissors" && computerChoice === "rock" ||
@@ -68,18 +38,6 @@ function playRound(humanChoice, computerChoice) {
         computerScore++;
         console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
-
-
-    // }else if () {
-    //     console.log("You lose! Scissors beat Paper");
-    //     computerScore++;
-    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
-
-
-    // }else if (humanChoice === "rock" && computerChoice === "paper") {
-    //     console.log("You lose! Paper beats Rock");
-    //     computerScore++;
-    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
 
     }else if (humanChoice === computerChoice) {
@@ -95,19 +53,6 @@ function playRound(humanChoice, computerChoice) {
 }
 
 
-
-
-
 for ( let i = 1; i <= 5; i++) {
     playRound(getHumanChoice(), getComputerChoice());     /* I did it!!!! I made the game by my own!!!!! WITHOUT ANY HELP OR CHEATING!!! */
-}  /*I know loops as well so used it, but I can do the way the project was asked as well */
-
-// function playGame() {
-//     playRound(getHumanChoice(), getComputerChoice());  
-//     playRound(getHumanChoice(), getComputerChoice());   /*I could've done it this way as well. */
-//     playRound(getHumanChoice(), getComputerChoice());  
-//     playRound(getHumanChoice(), getComputerChoice());  
-//     playRound(getHumanChoice(), getComputerChoice());
-// }
-
-// playGame();
+}
