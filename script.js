09d01a3997612a369da9e7604841854766405285
Lav <killerboylav4904@gmail.com>
2024-04-24 14:48:42 +0530

    5-round Rock-Paper-Scissors updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // console.log("Hello");
 
-let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+// let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
 // console.log(userChoice);
 
 function getHumanChoice() {
-    // let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+    let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
     // console.log(userChoice.toLowerCase());
     let user = userChoice.toLowerCase();
     // console.log(`User's input - ${user}`);
@@ -31,60 +31,83 @@ let humanScore = 0;
 let computerScore = 0; 
 
 
+// let humanSelection = getHumanChoice();           /*Commented it at last coz this was stopping me from getting new values for the playRound function */
+// let computerSelection = getComputerChoice();
+
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win! Rock beats Scissors");
+
+    if (humanChoice === "rock" && computerChoice === "scissors" || 
+        humanChoice === "scissors" && computerChoice === "paper" ||
+        humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You win as ${humanChoice} beats ${computerChoice}`);
         humanScore++;
         console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
+        
+
+    // }else if (humanChoice === "scissors" && computerChoice === "paper") {      /*This looks so messy lmao, would love to know how others did this part */
+    //     console.log("You win! Scissors beat Paper");
+    //     humanScore++;
+    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
 
-    }else if (humanChoice === "scissors" && computerChoice === "paper") {      /*This looks so messy lmao, would love to know how others did this part */
-        console.log("You win! Scissors beat Paper");
-        humanScore++;
-        console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
+    // }else if (humanChoice === "paper" && computerChoice === "rock") {
+    //     console.log("You win! Paper beats Rock");
+    //     humanScore++;
+    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
 
-    }else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats Rock");
-        humanScore++;
-        console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
+    /*I remembered that || can be used for "or" so tried doing this and it makes the code less lengthy. Not deleting the older codes as I would like to remember what code I wrote before xD */
 
-
-    }else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beats Scissors");
+    }else if (
+            humanChoice === "scissors" && computerChoice === "rock" ||
+            humanChoice === "paper" && computerChoice === "scissors" ||    
+            humanChoice === "rock" && computerChoice === "paper"
+        ) {
+        console.log(`You lose as ${computerChoice} beats ${humanChoice}`);
         computerScore++;
         console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
 
-    }else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Scissors beat Paper");
-        computerScore++;
-        console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
+
+    // }else if () {
+    //     console.log("You lose! Scissors beat Paper");
+    //     computerScore++;
+    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
 
-    }else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats Rock");
-        computerScore++;
-        console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
+    // }else if (humanChoice === "rock" && computerChoice === "paper") {
+    //     console.log("You lose! Paper beats Rock");
+    //     computerScore++;
+    //     console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
 
     }else if (humanChoice === computerChoice) {
         console.log("Draw! No points");
+        console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
 
         
     }else {
         console.log("No points");
+        console.log(`User score - ${humanScore}, Computer score - ${computerScore}`);
     }
 
 }
 
 
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
 
 
+for ( let i = 1; i <= 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());     /* I did it!!!! I made the game by my own!!!!! WITHOUT ANY HELP OR CHEATING!!! */
+}  /*I know loops as well so used it, but I can do the way the project was asked as well */
 
+// function playGame() {
+//     playRound(getHumanChoice(), getComputerChoice());  
+//     playRound(getHumanChoice(), getComputerChoice());   /*I could've done it this way as well. */
+//     playRound(getHumanChoice(), getComputerChoice());  
+//     playRound(getHumanChoice(), getComputerChoice());  
+//     playRound(getHumanChoice(), getComputerChoice());
+// }
+
+// playGame();
