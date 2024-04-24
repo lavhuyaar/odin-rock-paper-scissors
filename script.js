d44bc225f26f6f559645d6019ac45e6f47d9a5b1
Lav <killerboylav4904@gmail.com>
2024-04-24 13:00:57 +0530

    Comparing function added

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // console.log("Hello");
 
-// let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
 // console.log(userChoice);
 
 function getHumanChoice() {
-    let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
-    // console.log(userChoice);
-    return userChoice;
+    // let userChoice = prompt(`Choose any one of "rock", "paper" and "scissors" : `);
+    // console.log(userChoice.toLowerCase());
+    return userChoice.toLowerCase();
     
 }
 
@@ -20,3 +20,46 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+
+
+let humanScore = 0;
+let computerScore = 0; 
+
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors");
+        humanScore = humanScore + 1;
+    }else if (humanChoice === "scissors" && computerChoice === "paper") {      /*This looks so messy lmao, would love to know how others did this part */
+        console.log("You win! Scissors beats Paper");
+        humanScore = humanScore + 1;
+    }else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock");
+        humanScore = humanScore + 1;
+    }else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors");
+        computerScore = computerScore + 1;
+    }else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper");
+        computerScore = computerScore + 1;
+    }else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock");
+        computerScore = computerScore + 1;
+    }else if (humanChoice === computerChoice) {
+        console.log("Draw! No points");
+    }else console.log("No points");
+}
+
+
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
+
+console.log(humanSelection, computerSelection);
+console.log(humanScore, computerScore);
